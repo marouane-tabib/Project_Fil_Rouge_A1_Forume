820a766cd832f941f112ef3d85cc9365e1a19e6d
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-05 09:21:21 +0000

    Add the database project file, migration | factory | seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'category_id' => 1,
+            'primary_color_id' => 1,
+            'secondary_color_id' => 2,
+            'home_style_id' => 1,
+            'image' => fake()->image(),
+            'title' => fake()->title(),
+            'width' => fake()->numberBetween(2, 40),
+            'length' => fake()->numberBetween(2, 40),
+            'diameter' => fake()->numberBetween(2, 40),
+            'description' => fake()->text(400),
+            'price' => fake()->numberBetween(220, 430),
+            'quantity' => fake()->numberBetween(5, 40),
+            'SKU' => fake()->numberBetween(1, 9),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,29 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        \App\Models\User::factory(30)->create();
+        // \App\Models\User::factory(10)->create();
+        $this->call(CategorySeeder::class);
+        $this->call(ColorSeeder::class);
+        $this->call(HomeStyleSeeder::class);
+        $this->call(MaterailSeeder::class);
+        $this->call(PaternSeeder::class);
+        $this->call(RoomSeeder::class);
+        $this->call(ShapeSeeder::class);
+        $this->call(TagSeeder::class);
+        $this->call(ProductSeeder::class);
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
