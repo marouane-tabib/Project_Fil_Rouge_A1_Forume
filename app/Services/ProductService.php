91b36120eeafa06da9aab1b82204c4564ab26e68
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-05 12:18:39 +0000

    Make the ProductService and injecting the  ProductRepositoryInterface

---
+++ app/Services/ProductService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services;
+
+use App\Interfaces\ProductRepositoryInterface;
+
+class ProductService
+{
+  protected ProductRepositoryInterface $productRepository;
+
+  public function __construct(ProductRepositoryInterface $productRepository)
+  {
+    $this->productRepository = $productRepository;
+  }
+
+  public function all()
+  {
+    return $this->productRepository->all();
+  }
+
+  public function create(array $data)
+  {
+    return $this->productRepository->create($data);
+  }
+
+  public function update(int $id, array $data)
+  {
+    return $this->productRepository->update($id, $data);
+  }
+
+  public function delete(int $id)
+  {
+    return $this->productRepository->delete($id);
+  }
+}
