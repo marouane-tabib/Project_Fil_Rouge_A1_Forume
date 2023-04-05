dfef69784eab3b619cd6a389fd9b220537de97c2
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-05 10:54:37 +0000

    Injecting the Model class in the BaseRepository, Add the return in the all, create, update, delete, methods

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -3,26 +3,34 @@
 namespace App\BaseRepository;
 
 use App\BaseRepositoryInterface\BaseRepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
 
 class BaseRepository implements BaseRepositoryInterface
 {
+  private Model $model;
+
+  public function __construct(Model $model)
+  {
+    $this->model = $model;
+  }
+
   public function all()
   {
-    //
+    return $this->model->all();
   }
 
   public function create(array $data)
   {
-    //
+    return $this->model->create($data);
   }
 
   public function update(int $id, array $data)
   {
-    //
+    return $this->model->find($id)->update($data);
   }
 
   public function delete(int $id)
   {
-    // 
+    return $this->model->find($id)->delete();
   }
 }
