e056a929a912b211cc7f6e1005c5a2298fa3a802
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-05 12:14:43 +0000

    Bind the ProductRepositoryInterface with ProductRepository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Interfaces\BaseRepositoryInterface;
+use App\Interfaces\ProductRepositoryInterface;
 use App\Repositories\BaseRepository;
+use App\Repositories\ProductRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -16,6 +18,7 @@ class RepositoryServiceProvider extends ServiceProvider
     public function register()
     {
       $this->app->bind(BaseRepositoryInterface::class, BaseRepository::class);
+      $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
     }
 
     /**
