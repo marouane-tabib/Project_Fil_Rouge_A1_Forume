b18ac1b62e25e6ffac899daf515e99a46e73b804
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-05 11:17:26 +0000

    Inject BaseRepositoryInterface in the BaseCrudService | Add the methods in the BaseCrudService class

--- app/Services/BaseCrudService.php
+++ app/Services/BaseCrudService.php
@@ -2,7 +2,34 @@
 
 namespace App\Services\BaseCrudService;
 
+use App\BaseRepositoryInterface\BaseRepositoryInterface;
+
 class BaseCrudService
 {
-  // 
+  protected BaseRepositoryInterface $baseRepository;
+
+  public function __construct(BaseRepositoryInterface $baseRepository)
+  {
+    $this->baseRepository = $baseRepository;
+  }
+
+  public function all()
+  {
+    return $this->baseRepository->all();
+  }
+
+  public function create(array $data)
+  {
+    return $this->baseRepository->create($data);
+  }
+
+  public function update(int $id, array $data)
+  {
+    return $this->baseRepository->update($id, $data);
+  }
+
+  public function delete(int $id)
+  {
+    return $this->baseRepository->delete($id);
+  }
 }
