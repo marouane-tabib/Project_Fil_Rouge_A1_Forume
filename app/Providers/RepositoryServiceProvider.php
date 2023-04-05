52f4881edd97788b634155828bca7340d8720d6c
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-05 11:04:12 +0000

    Bind the BaseRepositoryInterface with BaseRepository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\BaseRepository\BaseRepository;
+use App\BaseRepositoryInterface\BaseRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+      $this->app->bind(BaseRepositoryInterface::class, BaseRepository::class);
     }
 
     /**
