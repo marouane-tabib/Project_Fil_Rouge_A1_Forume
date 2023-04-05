a147f8c65bdf20561762044ae864a852a0d8a6d6
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-05 10:38:34 +0000

    Add the BaseRepositoryInterface implements in the BaseRepository class

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -2,7 +2,27 @@
 
 namespace App\BaseRepository;
 
-class BaseRepository
+use App\BaseRepositoryInterface\BaseRepositoryInterface;
+
+class BaseRepository implements BaseRepositoryInterface
 {
-  // 
+  public function all()
+  {
+    //
+  }
+
+  public function create(array $data)
+  {
+    //
+  }
+
+  public function update(int $id, array $data)
+  {
+    //
+  }
+
+  public function delete(int $id)
+  {
+    // 
+  }
 }
