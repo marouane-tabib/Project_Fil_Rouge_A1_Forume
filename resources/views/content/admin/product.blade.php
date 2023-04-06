9df69bcbff9e8560a87d1d55047ddafac8457218
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-06 14:33:34 +0000

    Delete the Datatable from product balade, and add the table-classic

--- resources/views/content/admin/product.blade.php
+++ resources/views/content/admin/product.blade.php
@@ -34,77 +34,46 @@
 @endsection
 
 @section('content')
-<div class="card-datatable table-responsive pt-0">
-  <table class="datatables-basic table">
+<div class="table-responsive">
+  <table class="table">
     <thead>
       <tr>
-        <th></th>
-        <th></th>
-        <th>id</th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Date</th>
-        <th>Salary</th>
+        <th>Project</th>
+        <th>Client</th>
+        <th>Users</th>
         <th>Status</th>
-        <th>Action</th>
+        <th>Actions</th>
       </tr>
     </thead>
+    <tbody class="table-border-bottom-0">
+      <tr>
+        <td><i class="ti ti-brand-angular ti-lg text-danger me-3"></i> <strong>Angular Project</strong></td>
+        <td>Albert Cook</td>
+        <td>
+          <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
+            <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Lilian Fuller">
+              <img src="assets/img/avatars/5.png" alt="Avatar" class="rounded-circle">
+            </li>
+            <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Sophia Wilkerson">
+              <img src="assets/img/avatars/6.png" alt="Avatar" class="rounded-circle">
+            </li>
+            <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Christina Parker">
+              <img src="assets/img/avatars/7.png" alt="Avatar" class="rounded-circle">
+            </li>
+          </ul>
+        </td>
+        <td><span class="badge bg-label-primary me-1">Active</span></td>
+        <td>
+          <div class="dropdown">
+            <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical"></i></button>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-pencil me-1"></i>Edit</a>
+              <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-1"></i>Delete</a>
+            </div>
+          </div>
+        </td>
+      </tr>
+    </tbody>
   </table>
 </div>
-<!-- Modal to add new record -->
-<div class="offcanvas offcanvas-end" id="add-new-record">
-  <div class="offcanvas-header border-bottom">
-    <h5 class="offcanvas-title" id="exampleModalLabel">New Record</h5>
-    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body flex-grow-1">
-    <form class="add-new-record pt-0 row g-2" id="form-add-new-record" onsubmit="return false">
-      <div class="col-sm-12">
-        <label class="form-label" for="basicFullname">Full Name</label>
-        <div class="input-group input-group-merge">
-          <span id="basicFullname2" class="input-group-text"><i class="ti ti-user"></i></span>
-          <input type="text" id="basicFullname" class="form-control dt-full-name" name="basicFullname" placeholder="John Doe" aria-label="John Doe" aria-describedby="basicFullname2" />
-        </div>
-      </div>
-      <div class="col-sm-12">
-        <label class="form-label" for="basicPost">Post</label>
-        <div class="input-group input-group-merge">
-          <span id="basicPost2" class="input-group-text"><i class='ti ti-briefcase'></i></span>
-          <input type="text" id="basicPost" name="basicPost" class="form-control dt-post" placeholder="Web Developer" aria-label="Web Developer" aria-describedby="basicPost2" />
-        </div>
-      </div>
-      <div class="col-sm-12">
-        <label class="form-label" for="basicEmail">Email</label>
-        <div class="input-group input-group-merge">
-          <span class="input-group-text"><i class="ti ti-mail"></i></span>
-          <input type="text" id="basicEmail" name="basicEmail" class="form-control dt-email" placeholder="john.doe@example.com" aria-label="john.doe@example.com" />
-        </div>
-        <div class="form-text">
-          You can use letters, numbers & periods
-        </div>
-      </div>
-      <div class="col-sm-12">
-        <label class="form-label" for="basicDate">Joining Date</label>
-        <div class="input-group input-group-merge">
-          <span id="basicDate2" class="input-group-text"><i class='ti ti-calendar'></i></span>
-          <input type="text" class="form-control dt-date" id="basicDate" name="basicDate" aria-describedby="basicDate2" placeholder="MM/DD/YYYY" aria-label="MM/DD/YYYY" />
-        </div>
-      </div>
-      <div class="col-sm-12">
-        <label class="form-label" for="basicSalary">Salary</label>
-        <div class="input-group input-group-merge">
-          <span id="basicSalary2" class="input-group-text"><i class='ti ti-currency-dollar'></i></span>
-          <input type="number" id="basicSalary" name="basicSalary" class="form-control dt-salary" placeholder="12000" aria-label="12000" aria-describedby="basicSalary2" />
-        </div>
-      </div>
-      <div class="col-sm-12">
-        <button type="submit" class="btn btn-primary data-submit me-sm-3 me-1">Submit</button>
-        <button type="reset" class="btn btn-outline-secondary" data-bs-dismiss="offcanvas">Cancel</button>
-      </div>
-    </form>
-
-  </div>
-</div>
-<!--/ DataTable with Buttons -->
-
 @endsection
