7939dee8ec69ed7866371861c8bb2933595724a0
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-06 14:03:03 +0000

    clean

--- resources/views/content/admin/product.blade.php
+++ resources/views/content/admin/product.blade.php
@@ -30,10 +30,6 @@
 @endsection
 
 @section('page-script')
-{{-- <script>
-  var data = {!! $data !!}
-  console.log(data);
-</script> --}}
 <script src="{{asset('assets/js/tables-datatables-basic.js')}}"></script>
 @endsection
 
