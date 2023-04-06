6b845c3ad99c825fc579223be68df2d1d12a699c
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-06 10:19:31 +0000

    Change the Content of default blade file

--- resources/views/content/admine/default.blade.php
+++ resources/views/content/admine/default.blade.php
@@ -1,3 +1,13 @@
 @php
-   dd($data);
+ $configData = Helper::appClasses();
 @endphp
+
+@extends('layouts/layoutMaster')
+
+@section('title', 'Products')
+
+@section('content')
+  @foreach ($data as $item)
+    <li>$item->title</li>
+  @endforeach
+@endsection
