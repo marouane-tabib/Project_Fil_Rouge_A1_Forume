dbea2445b59859f5218934c2d5d8baac0099e801
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-07 12:10:42 +0000

    Add the destroy method in the ProductController

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -88,6 +88,7 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+      $this->productService->delete($id);
+      return redirect()->back();
     }
 }
