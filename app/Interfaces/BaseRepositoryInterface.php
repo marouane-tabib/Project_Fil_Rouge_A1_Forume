423e07ef6a15b315b101895a7fe24d5f65f66d87
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-09 16:58:15 +0000

    Add the find method in the BaseRepositoryInterface

--- app/Interfaces/BaseRepositoryInterface.php
+++ app/Interfaces/BaseRepositoryInterface.php
@@ -5,6 +5,7 @@ namespace App\Interfaces;
 interface BaseRepositoryInterface
 {
   public function all();
+  public function find(int $id);
   public function create(array $data);
   public function update(int $id, array $data);
   public function delete(int $id);
