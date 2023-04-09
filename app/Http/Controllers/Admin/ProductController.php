cc5f70d94ef6234a91276feb52a80f60445858c3
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-09 17:24:39 +0000

    Add the test update method to ProductController

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -77,7 +77,8 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+      $this->productService->update($id, $request->all);
+      return redirect()->back();
     }
 
     /**
