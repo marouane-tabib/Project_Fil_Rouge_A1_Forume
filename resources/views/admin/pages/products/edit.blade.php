dbc34827a6e276f19278ca9e1442da7e631608c9
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-09 23:46:21 +0000

    Add the form data to pages/products/edit

--- resources/views/admin/pages/products/edit.blade.php
+++ resources/views/admin/pages/products/edit.blade.php
@@ -50,123 +50,63 @@
       <div class="card-body">
         <div class="row">
           <div class="col-lg-8 mx-auto">
-            <!-- 1. Delivery Address -->
+            <!-- 1. Delivery Address | title, category, price, quantity, sku, primary color, secodary color, home style-->
             <h5 class="mb-4">1. Delivery Address</h5>
             <div class="row g-3">
-              <div class="col-md-6">
-                <label class="form-label" for="fullname">Full Name</label>
-                <input type="text" id="fullname" class="form-control" placeholder="John Doe" />
+              <div class="col-md-12">
+                <label class="form-label" for="title">Title</label>
+                <input type="text" id="title" class="form-control" placeholder="Add Your Title..." />
               </div>
-              <div class="col-md-6">
-                <label class="form-label" for="email">Email</label>
-                <div class="input-group input-group-merge">
-                  <input class="form-control" type="text" id="email" name="email" placeholder="john.doe" aria-label="john.doe" aria-describedby="email3" />
-                  <span class="input-group-text" id="email3">@example.com</span>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <label class="form-label" for="phone">Contact Number</label>
-                <input type="text" id="phone" class="form-control phone-mask" placeholder="658 799 8941" aria-label="658 799 8941" />
-              </div>
-              <div class="col-md-6">
-                <label class="form-label" for="alt-num">Alternate Number</label>
-                <input type="text" id="alt-num" class="form-control phone-mask" placeholder="658 799 8941" />
-              </div>
-              <div class="col-12">
-                <label class="form-label" for="address">Address</label>
-                <textarea name="address" class="form-control" id="address" rows="2" placeholder="1456, Mall Road"></textarea>
-              </div>
-              <div class="col-md-6">
-                <label class="form-label" for="pincode">Pincode</label>
-                <input type="text" id="pincode" class="form-control" placeholder="658468" />
-              </div>
-              <div class="col-md-6">
-                <label class="form-label" for="landmark">Landmark</label>
-                <input type="text" id="landmark" class="form-control" placeholder="Nr. Wall Street" />
-              </div>
-              <div class="col-md">
-                <label class="form-label" for="city">City</label>
-                <input type="text" id="city" class="form-control" placeholder="Jackson" />
-              </div>
-              <div class="col-md">
-                <label class="form-label" for="state">State</label>
-                <select id="state" class="select2 form-select" data-allow-clear="true">
+              <div class="col-md-12">
+                <label class="form-label" for="categories">Categoryies</label>
+                <select id="categories" class=" form-select" data-allow-clear="true">
                   <option value="">Select</option>
                   <option value="AL">Alabama</option>
                   <option value="AK">Alaska</option>
                   <option value="AZ">Arizona</option>
-                  <option value="AR">Arkansas</option>
-                  <option value="CA">California</option>
-                  <option value="CO">Colorado</option>
-                  <option value="CT">Connecticut</option>
-                  <option value="DE">Delaware</option>
-                  <option value="DC">District Of Columbia</option>
-                  <option value="FL">Florida</option>
-                  <option value="GA">Georgia</option>
-                  <option value="HI">Hawaii</option>
-                  <option value="ID">Idaho</option>
-                  <option value="IL">Illinois</option>
-                  <option value="IN">Indiana</option>
-                  <option value="IA">Iowa</option>
-                  <option value="KS">Kansas</option>
-                  <option value="KY">Kentucky</option>
-                  <option value="LA">Louisiana</option>
-                  <option value="ME">Maine</option>
-                  <option value="MD">Maryland</option>
-                  <option value="MA">Massachusetts</option>
-                  <option value="MI">Michigan</option>
-                  <option value="MN">Minnesota</option>
-                  <option value="MS">Mississippi</option>
-                  <option value="MO">Missouri</option>
-                  <option value="MT">Montana</option>
-                  <option value="NE">Nebraska</option>
-                  <option value="NV">Nevada</option>
-                  <option value="NH">New Hampshire</option>
-                  <option value="NJ">New Jersey</option>
-                  <option value="NM">New Mexico</option>
-                  <option value="NY">New York</option>
-                  <option value="NC">North Carolina</option>
-                  <option value="ND">North Dakota</option>
-                  <option value="OH">Ohio</option>
-                  <option value="OK">Oklahoma</option>
-                  <option value="OR">Oregon</option>
-                  <option value="PA">Pennsylvania</option>
-                  <option value="RI">Rhode Island</option>
-                  <option value="SC">South Carolina</option>
-                  <option value="SD">South Dakota</option>
-                  <option value="TN">Tennessee</option>
-                  <option value="TX">Texas</option>
-                  <option value="UT">Utah</option>
-                  <option value="VT">Vermont</option>
-                  <option value="VA">Virginia</option>
-                  <option value="WA">Washington</option>
-                  <option value="WV">West Virginia</option>
-                  <option value="WI">Wisconsin</option>
-                  <option value="WY">Wyoming</option>
                 </select>
               </div>
-              <div class="col-12">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" value="" id="deliveryAdd" checked="">
-                  <label class="form-check-label" for="deliveryAdd"> Use this as default delivery address </label>
-                </div>
+              <div class="col-md-12">
+                <label class="form-label" for="price">Price</label>
+                <input type="number" id="price" class="form-control phone-mask" placeholder="Add Your Product Price" />
               </div>
-
-              <label class="form-check-label">Address Type</label>
-              <div class="col mt-2">
-                <div class="form-check form-check-inline">
-                  <input name="collapsible-address-type" class="form-check-input" type="radio" value="" id="collapsible-address-type-home" checked="" />
-                  <label class="form-check-label" for="collapsible-address-type-home">Home (All day delivery)</label>
-                </div>
-                <div class="form-check form-check-inline">
-                  <input name="collapsible-address-type" class="form-check-input" type="radio" value="" id="collapsible-address-type-office" />
-                  <label class="form-check-label" for="collapsible-address-type-office"> Office (Delivery between 10 AM - 5 PM) </label>
-                </div>
+              <div class="col-md-12">
+                <label class="form-label" for="sku">SKU</label>
+                <input type="number" id="sku" class="form-control phone-mask" placeholder="Add Your Product SKU" />
               </div>
-            </div>
-            <hr>
+              <div class="col-md-12">
+                <label class="form-label" for="quantity">Quantity</label>
+                <input type="number" id="quantity" class="form-control phone-mask" placeholder="Add Your Product Quantity" />
+              </div>
+              <div class="col-md-12">
+                <label class="form-label" for="primary_color">Pripary Color</label>
+                <select id="primary_color" class=" form-select" data-allow-clear="true">
+                  <option value="">Select</option>
+                  <option value="AL">Alabama</option>
+                  <option value="AK">Alaska</option>
+                  <option value="AZ">Arizona</option>
+                </select>
+              </div>
+              <div class="col-md-12">
+                <label class="form-label" for="secondary_color">Secondary Color</label>
+                <select id="secondary_color" class=" form-select" data-allow-clear="true">
+                  <option value="">Select</option>
+                  <option value="AL">Alabama</option>
+                  <option value="AK">Alaska</option>
+                  <option value="AZ">Arizona</option>
+                </select>
+              </div>
+              <div class="col-md-12">
+                <label class="form-label" for="home_style">Home Style</label>
+                <select id="home_style" class=" form-select" data-allow-clear="true">
+                  <option value="">Select</option>
+                  <option value="AL">Alabama</option>
+                  <option value="AK">Alaska</option>
+                  <option value="AZ">Arizona</option>
+                </select>
+              </div>
             <!-- 2. Delivery Type -->
-            <h5 class="my-4">2. Delivery Type</h5>
+            {{-- <h5 class="my-4">2. Delivery Type</h5>
             <div class="row gy-3">
               <div class="col-md">
                 <div class="form-check custom-option custom-option-icon">
@@ -204,9 +144,9 @@
                   </label>
                 </div>
               </div>
-            </div>
+            </div> --}}
             <hr>
-            <!-- 3. Apply Promo code -->
+            {{-- <!-- 3. Apply Promo code -->
             <h5 class="my-4">3. Apply Promo code</h5>
             <div class="row g-3">
 
@@ -246,9 +186,9 @@
                   </li>
                 </ul>
               </div>
-            </div>
+            </div> --}}
             <hr>
-            <!-- 4. Payment Method -->
+            {{-- <!-- 4. Payment Method -->
             <h5 class="my-4">4. Payment Method</h5>
             <div class="row g-3">
               <div class="mb-3">
@@ -299,7 +239,7 @@
                   </div>
                 </div>
               </div>
-            </div>
+            </div> --}}
           </div>
         </div>
       </div>
