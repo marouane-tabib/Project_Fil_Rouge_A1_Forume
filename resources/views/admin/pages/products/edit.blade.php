62d39f8e80023c16262bd9ec8ccab68b3a7beac7
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-09 17:16:45 +0000

    Add sum data to pages/product/edit balade file

--- resources/views/admin/pages/products/edit.blade.php
+++ resources/views/admin/pages/products/edit.blade.php
@@ -4,7 +4,7 @@
 
 @extends('admin/layouts/layoutMaster')
 
-@section('title', 'Products Edit')
+@section('title', 'Products Edit'. $product->name)
 
 @section('vendor-style')
 <link rel="stylesheet" href="{{asset('assets/vendor/libs/select2/select2.css')}}" />
@@ -25,17 +25,18 @@
 
 <h4 class="fw-bold py-3 mb-4">
   <span class="text-muted fw-light">Forms/</span>
-  Sticky Actions
+  Edit {{ $product->title }}
 </h4>
 <!-- Sticky Actions -->
 <div class="row">
   <div class="col-12">
     <div class="card">
       <div class="card-header sticky-element bg-label-secondary d-flex justify-content-sm-between align-items-sm-center flex-column flex-sm-row">
-        <h5 class="card-title mb-sm-0 me-2">Sticky Action Bar</h5>
+        <h5 class="card-title mb-sm-0 me-2">{{ $product->title }}</h5>
         <div class="action-btns">
           <button class="btn btn-label-primary me-3">
-            <span class="align-middle"> Back</span>
+            <a href="{{ URL::previous() }}">
+            <span class="align-middle"> Back</span></a>
           </button>
           <button class="btn btn-primary">
             Place Order
