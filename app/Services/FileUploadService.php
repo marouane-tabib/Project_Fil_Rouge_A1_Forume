0a7d64aed16dc103dbd04f5d248134a6bd8f61c5
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-10 15:39:35 +0000

    Add the upoadFile method in the FileUploadService - completed

--- app/Services/FileUploadService.php
+++ app/Services/FileUploadService.php
@@ -4,8 +4,17 @@ namespace App\Services;
 
 class FileUploadService
 {
-  public function uploadFile(){
-    //
+  public function uploadFile($file, $baseDerictory, $childDerictory){
+    // Generate file name
+    $filename = date('YmdHi').$file->getClientOriginalName();
+    // Create the storage derictory link
+    $storagePath = public_path($baseDerictory.$childDerictory.'/'.date('m-Y'));
+    // Generate the storage derictory child link to this file
+    $storagePathChild = date('m-Y').'/'.$filename;
+    // Move to store the file
+    $file->move($storagePath, $filename);
+    // return the finale database storing name
+    return $storagePathChild;
   }
 
   public function deleteFile(){
