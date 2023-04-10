06a5334dad58c24d520aec9ee385dd166e27fd2f
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-10 00:02:00 +0000

    Add the horisontal form to pages/products/edit

--- resources/views/admin/pages/products/edit.blade.php
+++ resources/views/admin/pages/products/edit.blade.php
@@ -7,13 +7,15 @@
 @section('title', 'Products Edit'. $product->name)
 
 @section('vendor-style')
+<link rel="stylesheet" href="{{asset('assets/vendor/libs/flatpickr/flatpickr.css')}}" />
 <link rel="stylesheet" href="{{asset('assets/vendor/libs/select2/select2.css')}}" />
 @endsection
 
 @section('vendor-script')
-<script src="{{asset('assets/vendor/libs/jquery-sticky/jquery-sticky.js')}}"></script>
 <script src="{{asset('assets/vendor/libs/cleavejs/cleave.js')}}"></script>
 <script src="{{asset('assets/vendor/libs/cleavejs/cleave-phone.js')}}"></script>
+<script src="{{asset('assets/vendor/libs/moment/moment.js')}}"></script>
+<script src="{{asset('assets/vendor/libs/flatpickr/flatpickr.js')}}"></script>
 <script src="{{asset('assets/vendor/libs/select2/select2.js')}}"></script>
 @endsection
 
@@ -23,93 +25,121 @@
 
 @section('content')
 
-<h4 class="fw-bold py-3 mb-4">
-  <span class="text-muted fw-light">Forms/</span>
-  Edit {{ $product->title }}
-</h4>
-<!-- Sticky Actions -->
-<div class="row">
-  <div class="col-12">
-    <div class="card">
-      <div class="card-header sticky-element bg-label-secondary d-flex justify-content-sm-between align-items-sm-center flex-column flex-sm-row">
-        <h5 class="card-title mb-sm-0 me-2">{{ $product->title }}</h5>
-        <form action="{{ route('product.update', $product->id)}}" method="post">
-          @csrf
-          @method('PUT')
-          <div class="action-btns">
-            <button class="btn btn-label-primary me-3">
-              <a href="{{ URL::previous() }}">
-              <span class="align-middle"> Back</span></a>
-            </button>
-              <button class="btn btn-primary" type="submit">
-                Update
-              </button>
+  <!-- Basic Layout -->
+  <div class="col-xxl">
+    <div class="card mb-4">
+      <div class="card-header d-flex align-items-center justify-content-between">
+        <h5 class="mb-0">Edit {{ $product->title }}</h5>
+      </div>
+      <div class="card-body">
+        <form>
+          <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for="title">Title</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="title" placeholder="Add product title..." />
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for="price">Price</label>
+            <div class="col-sm-10">
+              <input type="number" class="form-control" id="price" placeholder="Add product price..." />
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for="quantity">Quantity</label>
+            <div class="col-sm-10">
+              <input type="number" class="form-control" id="quantity" placeholder="Add product quantity..." />
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for="sku">SKU</label>
+            <div class="col-sm-10">
+              <input type="number" class="form-control" id="sku" placeholder="Add product sku..." />
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for="categories">Categories</label>
+            <div class="col-sm-10">
+              <select id="categories" class="select2 form-select" data-allow-clear="true">
+                <option value="">Select</option>
+                <option value="Australia">Australia</option>
+                <option value="Bangladesh">Bangladesh</option>
+                <option value="Belarus">Belarus</option>
+                <option value="Brazil">Brazil</option>
+              </select>
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for="primary-color">Primary Color</label>
+            <div class="col-sm-10">
+              <select id="primary-color" class="select2 form-select" data-allow-clear="true">
+                <option value="">Select</option>
+                <option value="Australia">Australia</option>
+                <option value="Bangladesh">Bangladesh</option>
+                <option value="Belarus">Belarus</option>
+                <option value="Brazil">Brazil</option>
+              </select>
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for="secondary-color">Secondary Color</label>
+            <div class="col-sm-10">
+              <select id="secondary-color" class="select2 form-select" data-allow-clear="true">
+                <option value="">Select</option>
+                <option value="Australia">Australia</option>
+                <option value="Bangladesh">Bangladesh</option>
+                <option value="Belarus">Belarus</option>
+                <option value="Brazil">Brazil</option>
+              </select>
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for="home-style">Home Style</label>
+            <div class="col-sm-10">
+              <select id="home-style" class="select2 form-select" data-allow-clear="true">
+                <option value="">Select</option>
+                <option value="Australia">Australia</option>
+                <option value="Bangladesh">Bangladesh</option>
+                <option value="Belarus">Belarus</option>
+                <option value="Brazil">Brazil</option>
+              </select>
+            </div>
+          </div>
+          {{-- <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for="basic-default-company">Company</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="basic-default-company" placeholder="ACME Inc." />
+            </div>
+          </div> --}}
+          {{-- <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for="basic-default-email">Email</label>
+            <div class="col-sm-10">
+              <div class="input-group input-group-merge">
+                <input type="text" id="basic-default-email" class="form-control" placeholder="john.doe" aria-label="john.doe" aria-describedby="basic-default-email2" />
+                <span class="input-group-text" id="basic-default-email2">@example.com</span>
+              </div>
+              <div class="form-text"> You can use letters, numbers & periods </div>
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for="basic-default-phone">Phone No</label>
+            <div class="col-sm-10">
+              <input type="text" id="basic-default-phone" class="form-control phone-mask" placeholder="658 799 8941" aria-label="658 799 8941" aria-describedby="basic-default-phone" />
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for="basic-default-message">Message</label>
+            <div class="col-sm-10">
+              <textarea id="basic-default-message" class="form-control" placeholder="Hi, Do you have a moment to talk Joe?" aria-label="Hi, Do you have a moment to talk Joe?" aria-describedby="basic-icon-default-message2"></textarea>
+            </div>
+          </div> --}}
+          <div class="row justify-content-end">
+            <div class="col-sm-10">
+              <button type="submit" class="btn btn-primary">Update</button>
+            </div>
           </div>
         </form>
       </div>
-      <div class="card-body">
-        <div class="row">
-          <div class="col-lg-8 mx-auto">
-            <!-- 1. Delivery Address | title, category, price, quantity, sku, primary color, secodary color, home style-->
-            <h5 class="mb-4">1. Delivery Address</h5>
-            <div class="row g-3">
-              <div class="col-md-12">
-                <label class="form-label" for="title">Title</label>
-                <input type="text" id="title" class="form-control" placeholder="Add Your Title..." />
-              </div>
-              <div class="col-md-12">
-                <label class="form-label" for="categories">Categoryies</label>
-                <select id="categories" class=" form-select" data-allow-clear="true">
-                  <option value="">Select</option>
-                  <option value="AL">Alabama</option>
-                  <option value="AK">Alaska</option>
-                  <option value="AZ">Arizona</option>
-                </select>
-              </div>
-              <div class="col-md-12">
-                <label class="form-label" for="price">Price</label>
-                <input type="number" id="price" class="form-control phone-mask" placeholder="Add Your Product Price" />
-              </div>
-              <div class="col-md-12">
-                <label class="form-label" for="sku">SKU</label>
-                <input type="number" id="sku" class="form-control phone-mask" placeholder="Add Your Product SKU" />
-              </div>
-              <div class="col-md-12">
-                <label class="form-label" for="quantity">Quantity</label>
-                <input type="number" id="quantity" class="form-control phone-mask" placeholder="Add Your Product Quantity" />
-              </div>
-              <div class="col-md-12">
-                <label class="form-label" for="primary_color">Pripary Color</label>
-                <select id="primary_color" class=" form-select" data-allow-clear="true">
-                  <option value="">Select</option>
-                  <option value="AL">Alabama</option>
-                  <option value="AK">Alaska</option>
-                  <option value="AZ">Arizona</option>
-                </select>
-              </div>
-              <div class="col-md-12">
-                <label class="form-label" for="secondary_color">Secondary Color</label>
-                <select id="secondary_color" class=" form-select" data-allow-clear="true">
-                  <option value="">Select</option>
-                  <option value="AL">Alabama</option>
-                  <option value="AK">Alaska</option>
-                  <option value="AZ">Arizona</option>
-                </select>
-              </div>
-              <div class="col-md-12">
-                <label class="form-label" for="home_style">Home Style</label>
-                <select id="home_style" class=" form-select" data-allow-clear="true">
-                  <option value="">Select</option>
-                  <option value="AL">Alabama</option>
-                  <option value="AK">Alaska</option>
-                  <option value="AZ">Arizona</option>
-                </select>
-              </div>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
-</div>
-<!-- /Sticky Actions -->
 @endsection
