52c756880a7de77b02cd4d1fae69df41a987d856
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-10 14:45:29 +0000

    Add the values of the section and add sume inputs

--- resources/views/admin/pages/products/edit.blade.php
+++ resources/views/admin/pages/products/edit.blade.php
@@ -35,71 +35,78 @@
         <form method="POST" action="{{ route('product.update', $product->id)}}">
           @csrf
           @method('PUT')
+          {{-- <div class="mb-3">
+            <label class="form-label" for="image">Image</label>
+            <input type="file" class="form-control" id="image" name="image" placeholder="Add your product image" />
+          </div> --}}
           <div class="mb-3">
             <label class="form-label" for="title">Title</label>
-            <input type="text" class="form-control" id="title" value="{{ $product->title }}" name="title" placeholder="Add your product title" />
+            <input type="text" class="form-control" id="title" name="title" value="{{ $product->title }}" placeholder="Add your product title" />
           </div>
           <div class="mb-3">
             <label class="form-label" for="price">Price</label>
-            <input type="number" class="form-control" id="price" value="{{ $product->price }}" name="price" placeholder="Add your product price" />
+            <input type="number" class="form-control" id="price" name="price" value="{{ $product->price }}" placeholder="Add your product price" />
           </div>
           <div class="mb-3">
             <label class="form-label" for="quantity">Quantity</label>
-            <input type="number" class="form-control" id="quantity" value="{{ $product->quantity }}" name="quantity" placeholder="Add your product quantity" />
+            <input type="number" class="form-control" id="quantity" name="quantity" value="{{ $product->quantity }}" placeholder="Add your product quantity" />
           </div>
           <div class="mb-3">
             <label class="form-label" for="sku">SKU</label>
-            <input type="number" class="form-control" id="sku" value="{{ $product->sku }}" name="sku" placeholder="Add your product sku" />
+            <input type="number" class="form-control" id="sku" name="sku" value="{{ $product->sku }}" placeholder="Add your product sku" />
+          </div>
+          <div class="mb-3">
+            <label class="form-label" for="width">Width</label>
+            <input type="number" class="form-control" id="width" name="width" value="{{ $product->width }}" placeholder="Add your product width" />
+          </div>
+          <div class="mb-3">
+            <label class="form-label" for="length">Length</label>
+            <input type="number" class="form-control" id="length" name="length" value="{{ $product->length }}" placeholder="Add your product length" />
           </div>
           <div class="mb-3">
             <label class="form-label" for="categories">Categories</label>
-            <select id="categories" class="select2 form-select" name="categories" data-allow-clear="true">
-              <option value="">Select</option>
-              <option value="AL">Alabama</option>
-              <option value="AK">Alaska</option>
-              <option value="AZ">Arizona</option>
-              <option value="WV">West Virginia</option>
-              <option value="WI">Wisconsin</option>
-              <option value="WY">Wyoming</option>
+            <select id="categories" class="select2 form-select" name="category_id" data-allow-clear="true">
+              <option value="1">Select</option>
+              <option value="1">Alabama</option>
+              <option value="2">Alaska</option>
+              <option value="3">Wyoming</option>
             </select>
           </div>
           <div class="mb-3">
             <label class="form-label" for="primary-color">Primary Color</label>
-            <select id="primary-color" class="select2 form-select" name="primary_color" data-allow-clear="true">
-              <option value="">Select</option>
-              <option value="AL">Alabama</option>
-              <option value="AK">Alaska</option>
-              <option value="AZ">Arizona</option>
-              <option value="WV">West Virginia</option>
-              <option value="WI">Wisconsin</option>
-              <option value="WY">Wyoming</option>
+            <select id="primary-color" class="select2 form-select" name="primary_color_id" data-allow-clear="true">
+              <option value="1">Select</option>
+              <option value="1">Alabama</option>
+              <option value="2">Wisconsin</option>
+              <option value="3">Wyoming</option>
             </select>
           </div>
           <div class="mb-3">
             <label class="form-label" for="secondary-color">Secondary Color</label>
-            <select id="secondary-color" class="select2 form-select" name="secondary_color" data-allow-clear="true">
-              <option value="">Select</option>
-              <option value="AL">Alabama</option>
-              <option value="AK">Alaska</option>
-              <option value="AZ">Arizona</option>
-              <option value="WV">West Virginia</option>
-              <option value="WI">Wisconsin</option>
-              <option value="WY">Wyoming</option>
+            <select id="secondary-color" class="select2 form-select" name="secondary_color_id" data-allow-clear="true">
+              <option value="1">Select</option>
+              <option value="1">Alabama</option>
+              <option value="2">Alaska</option>
+              <option value="3">Arizona</option>
+              <option value="4">Wyoming</option>
             </select>
           </div>
           <div class="mb-3">
             <label class="form-label" for="home-style">Home Style</label>
-            <select id="home-style" class="select2 form-select" name="home_style" data-allow-clear="true">
-              <option value="">Select</option>
-              <option value="AL">Alabama</option>
-              <option value="AK">Alaska</option>
-              <option value="AZ">Arizona</option>
-              <option value="WV">West Virginia</option>
-              <option value="WI">Wisconsin</option>
-              <option value="WY">Wyoming</option>
+            <select id="home-style" class="select2 form-select" name="home_style_id" data-allow-clear="true">
+              <option value="1">Select</option>
+              <option value="1">Alabama</option>
+              <option value="2">Alaska</option>
+              <option value="3">Wisconsin</option>
+              <option value="4">Wyoming</option>
             </select>
           </div>
-          <button type="submit" class="btn btn-primary">Update</button>
+          <div class="mb-3">
+            <label class="form-label" for="description">Description</label>
+            {{-- <input type="number" class="form-control" id="description" name="description" placeholder="Add your product description" /> --}}
+            <textarea name="description" id="description" class="form-control" cols="30" rows="6" placeholder="Add your product description...">{{ $product->description }}</textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Create</button>
         </form>
       </div>
     </div>
