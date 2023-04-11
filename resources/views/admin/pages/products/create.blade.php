84fd570e4c80a471749fd13210ee3895ad712a81
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-11 15:08:09 +0000

    Clean the admin/pages/products/create

--- resources/views/admin/pages/products/create.blade.php
+++ resources/views/admin/pages/products/create.blade.php
@@ -131,59 +131,15 @@
 </div>
 
 <form id="formValidationExamples" class="row g-3">
-
-  <!-- Account Details -->
-
-  {{-- <div class="col-12">
-    <h5 class="fw-semibold">1. Account Details</h5>
-    <hr class="mt-0" />
-  </div> --}}
-
-
   <div class="col-md-6">
     <label class="form-label" for="formValidationName">Full Name</label>
     <input type="text" id="formValidationName" class="form-control" placeholder="John Doe" name="formValidationName" />
   </div>
-  {{-- <div class="col-md-6">
-    <label class="form-label" for="formValidationEmail">Email</label>
-    <input class="form-control" type="email" id="formValidationEmail" name="formValidationEmail" placeholder="john.doe" />
-  </div> --}}
-
-  {{-- <div class="col-md-6">
-    <div class="form-password-toggle">
-      <label class="form-label" for="formValidationPass">Password</label>
-      <div class="input-group input-group-merge">
-        <input class="form-control" type="password" id="formValidationPass" name="formValidationPass" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="multicol-password2" />
-        <span class="input-group-text cursor-pointer" id="multicol-password2"><i class="ti ti-eye-off"></i></span>
-      </div>
-    </div>
-  </div>
-  <div class="col-md-6">
-    <div class="form-password-toggle">
-      <label class="form-label" for="formValidationConfirmPass">Confirm Password</label>
-      <div class="input-group input-group-merge">
-        <input class="form-control" type="password" id="formValidationConfirmPass" name="formValidationConfirmPass" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="multicol-confirm-password2" />
-        <span class="input-group-text cursor-pointer" id="multicol-confirm-password2"><i class="ti ti-eye-off"></i></span>
-      </div>
-    </div>
-  </div> --}}
-
-
-  <!-- Personal Info -->
-
-  {{-- <div class="col-12">
-    <h5 class="mt-2 fw-semibold">2. Personal Info</h5>
-    <hr class="mt-0" />
-  </div> --}}
 
   <div class="col-md-6">
     <label for="formValidationFile" class="form-label">Profile Pic</label>
     <input class="form-control" type="file" id="formValidationFile" name="formValidationFile" placeholder="Choose profile pic">
   </div>
-  {{-- <div class="col-md-6">
-    <label class="form-label" for="formValidationDob">DOB</label>
-    <input type="text" class="form-control flatpickr-validation" name="formValidationDob" id="formValidationDob" required />
-  </div> --}}
 
   <div class="col-md-6">
     <label class="form-label" for="formValidationSelect2">Country</label>
@@ -196,86 +152,16 @@
     </select>
   </div>
 
-
   <div class="col-md-6">
     <label class="form-label" for="formValidationBio">Bio</label>
     <textarea class="form-control" id="formValidationBio" name="formValidationBio" rows="3"></textarea>
   </div>
-  {{-- <div class="col-md-6">
-    <label class="form-label">Gender</label>
-    <div class="form-check custom mb-2">
-      <input type="radio" id="formValidationGender" name="formValidationGender" class="form-check-input" />
-      <label class="form-check-label" for="formValidationGender">Male</label>
-    </div>
-
-    <div class="form-check custom">
-      <input type="radio" id="formValidationGender2" name="formValidationGender" class="form-check-input" />
-      <label class="form-check-label" for="formValidationGender2">Female</label>
-    </div>
-  </div> --}}
-
-
-  <!-- Choose Your Plan -->
 
   <div class="col-12">
     <h5 class="mt-2 fw-semibold">3. Choose Your Plan</h5>
     <hr class="mt-0" />
   </div>
-  {{-- <div class="row">
-    <div class="col-xl-3 col-md-5 col-sm-6 col-12">
-      <div class="form-check custom-option custom-option-icon">
-        <label class="form-check-label custom-option-content" for="basicPlanMain1">
-          <span class="custom-option-body">
-            <i class="ti ti-brand-telegram"></i>
-            <span class="custom-option-title"> Starter </span>
-            <small> Get 5gb of space and 1 team member. </small>
-          </span>
-          <input name="formValidationPlan" class="form-check-input" type="radio" value="" id="basicPlanMain1" />
-        </label>
-      </div>
-    </div>
-    <div class="col-xl-3 col-md-5 col-sm-6 col-12">
-      <div class="form-check custom-option custom-option-icon">
-        <label class="form-check-label custom-option-content" for="basicPlanMain2">
-          <span class="custom-option-body">
-            <i class="ti ti-users"></i>
-            <span class="custom-option-title"> Personal </span>
-            <small> Get 15gb of space and 5 team member. </small>
-          </span>
-          <input name="formValidationPlan" class="form-check-input" type="radio" value="" id="basicPlanMain2" />
-        </label>
-      </div>
-    </div>
-    <div class="col-xl-3 col-md-5 col-sm-6 col-12">
-      <div class="form-check custom-option custom-option-icon">
-        <label class="form-check-label custom-option-content" for="basicPlanMain3">
-          <span class="custom-option-body">
-            <i class="ti ti-crown"></i>
-            <span class="custom-option-title"> Premium </span>
-            <small> Get 25gb of space and 15 members. </small>
-          </span>
-          <input name="formValidationPlan" class="form-check-input" type="radio" value="" id="basicPlanMain3" />
-        </label>
-      </div>
-    </div>
-  </div> --}}
-
-  {{-- <div class="col-12">
-    <label class="switch switch-primary">
-      <input type="checkbox" class="switch-input" name="formValidationSwitch" />
-      <span class="switch-toggle-slider">
-        <span class="switch-on"></span>
-        <span class="switch-off"></span>
-      </span>
-      <span class="switch-label">Send me related emails</span>
-    </label>
-  </div> --}}
-  {{-- <div class="col-12">
-    <div class="form-check">
-      <input type="checkbox" class="form-check-input" id="formValidationCheckbox" name="formValidationCheckbox" />
-      <label class="form-check-label" for="formValidationCheckbox">Agree to our terms and conditions</label>
-    </div>
-  </div> --}}
+  
   <div class="col-12">
     <button type="submit" name="submitButton" class="btn btn-primary">Submit</button>
   </div>
