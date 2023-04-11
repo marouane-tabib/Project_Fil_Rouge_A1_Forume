9f5181612ddda47301b9bfa5861fff0ccafc58a0
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-11 09:28:42 +0000

    Add the uploadFile method in the update of to ProductService and uploadFile in the create mthod

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -27,12 +27,14 @@ class ProductService
 
   public function create(array $data)
   {
-    $data['image'] = $this->fileUpload->uploadFile($data['image'], 'storage/image/', 'product/');
+    $data['image'] = $this->fileUpload->uploadFile($data['image'], 'public/images/', 'products/');
     return $this->productRepository->create($data);
   }
 
   public function update(int $id, array $data)
   {
+    $product = $this->productRepository->find($id);
+    $data['image'] = $this->fileUpload->updateFile($product->image, $data['image'], 'public/images/', 'products/');
     return $this->productRepository->update($id, $data);
   }
 
