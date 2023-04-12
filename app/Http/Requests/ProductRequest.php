e7543b1a16b18a66dd82435fea194c20e487bdb4
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-12 21:58:14 +0000

    Add the switch toi check request rules method to return, add the POST and PUT method

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -13,7 +13,7 @@ class ProductRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,8 +23,48 @@ class ProductRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            //
-        ];
+      switch ($this->method()){
+          case 'POST' : {
+            return [
+              // Images types and size => Thumbnail images=>max:50KB|Product images=>max:500KB|Zoom or gallery images=>max:1MB
+              'image' => 'required|image|between:10,200|mimes:jpeg,png,jpg',
+              'title' => 'required|string|min:4|max:50',
+              'price' => 'required|numeric|min:50',
+              'quantity' => 'required|numeric|min:1',
+              'sku' => 'required|numeric|min:1',
+              'width' => 'required|numeric|min:0',
+              'length_value' => 'required|numeric|min:0',
+              'category_id' => 'required|numeric|exists:categories,id',
+              'primary_color_id' => 'required|numeric|exists:colors,id',
+              'secondary_color_id' => 'required|numeric|exists:colors,id',
+              'home_style_id' => 'required|numeric|exists:home_styles,id',
+              'description' => 'required|string|min:10|max:5000',
+            ];
+          }
+          case 'PUT' : {
+
+          }
+          case 'PATCH' : {
+            return [
+              // Images types and size => Thumbnail images=>max:50KB|Product images=>max:500KB|Zoom or gallery images=>max:1MB
+              'image' => 'nullable|image|between:10,200|mimes:jpeg,png,jpg',
+              'title' => 'required|string|min:4|max:50',
+              'price' => 'required|numeric|min:50',
+              'quantity' => 'required|numeric|min:1',
+              'sku' => 'required|numeric|min:1',
+              'width' => 'required|numeric|min:0',
+              'length_value' => 'required|numeric|min:0',
+              'category_id' => 'required|numeric|exists:categories,id',
+              'primary_color_id' => 'required|numeric|exists:colors,id',
+              'secondary_color_id' => 'required|numeric|exists:colors,id',
+              'home_style_id' => 'required|numeric|exists:home_styles,id',
+              'description' => 'required|string|min:10|max:5000',
+            ];
+          }
+          default: break;
+      }
+      return [
+          //
+      ];
     }
 }
