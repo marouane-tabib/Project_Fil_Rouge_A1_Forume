cb302fa3ea18e3b7ae077b0dc448d907219aeef6
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-16 03:06:33 +0000

    Add the handle method body in the RepositoryBinderCommand to add custom bind line in te RepositoryServiceProvider file

--- app/Console/Commands/RepositoryBinderCommand.php
+++ app/Console/Commands/RepositoryBinderCommand.php
@@ -25,8 +25,34 @@ class RepositoryBinderCommand extends Command
      *
      * @return int
      */
-    public function handle()
-    {
-        return Command::SUCCESS;
-    }
+
+     public function handle()
+     {
+         $providerFile = app_path('Providers/RepositoryServiceProvider.php');
+         $providerContents = file_get_contents($providerFile);
+
+         // Check if the bindings already exist
+         $bindings = [
+             BaseRepositoryInterface::class => BaseRepository::class,
+             ProductRepositoryInterface::class => ProductRepository::class,
+             // Add more bindings here as needed
+         ];
+         foreach ($bindings as $interface => $implementation) {
+             if (strpos($providerContents, "->bind($interface)") !== false) {
+                 $this->info("Binding for $interface already exists in RepositoryServiceProvider.");
+                 continue;
+             }
+
+             // Add the binding to the register() method
+             $binding = "\$this->app->bind($interface::class, $implementation::class);";
+             $providerContents = preg_replace(
+                 '/public function register\(\)\s*{/',
+                 "public function register()\n    {\n        $binding\n", // Add the binding after the opening brace
+                 $providerContents
+             );
+             $this->info("Binding for $interface has been added to RepositoryServiceProvider in the register() method.");
+         }
+
+         file_put_contents($providerFile, $providerContents);
+     }
 }
