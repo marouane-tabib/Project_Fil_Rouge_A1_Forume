e5ef3448415390baa710491582d53137ca505b66
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-21 00:42:51 +0000

    Make the CategoryService.php file

---
+++ app/Services/CategoryService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+use App\Interfaces\CategoryRepositoryInterface;
+
+class CategoryService
+{
+  protected CategoryRepositoryInterface $categoryRepository;
+
+  public function __construct(CategoryRepositoryInterface $categoryRepository)
+  {
+    $this->categoryRepository = $categoryRepository;
+  }
+}
