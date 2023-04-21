ac06733fcec17d66a1332aef20653813dcdd936f
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-21 01:01:50 +0000

    Add the view return in the index method in the CategoryController

--- app/Http/Controllers/AdminControllers/CategoryController.php
+++ app/Http/Controllers/AdminControllers/CategoryController.php
@@ -21,7 +21,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+      return view('admin.pages.categories.index', ['categories' => $this->categoryService->get()]);
     }
 
     /**
