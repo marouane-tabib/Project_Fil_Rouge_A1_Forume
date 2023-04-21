49bb94633790035147488273ed102d4fec0d2fb5
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-21 16:05:10 +0000

    Add the methods all|get|find|create|update|delete in the Service/TagService

--- app/Services/TagService.php
+++ app/Services/TagService.php
@@ -12,4 +12,34 @@ class TagService
   {
     $this->tagRepository = $tagRepository;
   }
+
+  public function all()
+  {
+    return $this->tagRepository->all();
+  }
+
+  public function get(array $select = ['*'], array $with = [])
+  {
+    return $this->tagRepository->get($select, $with);
+  }
+
+  public function find(int $id)
+  {
+    return $this->tagRepository->find($id);
+  }
+
+  public function create(array $data)
+  {
+    return $this->tagRepository->create($data);
+  }
+
+  public function update(int $id, array $data)
+  {
+    return $this->tagRepository->update($id, $data);
+  }
+
+  public function delete(int $id)
+  {
+    return $this->tagRepository->delete($id);
+  }
 }
