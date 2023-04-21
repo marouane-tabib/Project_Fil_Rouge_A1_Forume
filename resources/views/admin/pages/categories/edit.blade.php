1613ad9d038c0d2d523c696711ee21f240a07b4f
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-21 03:39:29 +0000

    Make the view/admin/../categories/edit.blade.php file

---
+++ resources/views/admin/pages/categories/edit.blade.php
@@ -0,0 +1,25 @@
+@php
+ $configData = Helper::appClasses();
+@endphp
+
+@extends('admin/layouts/layoutMaster')
+
+@section('title', 'Categories')
+
+@section('vendor-style')
+<link rel="stylesheet" href="{{asset('assets/vendor/libs/formvalidation/dist/css/formValidation.min.css')}}" />
+@endsection
+
+@section('vendor-script')
+<script src="{{asset('assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js')}}"></script>
+<script src="{{asset('assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js')}}"></script>
+<script src="{{asset('assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js')}}"></script>
+@endsection
+
+@section('page-script')
+<script src="{{asset('admin/assets/js/categories/data-form-validation.js')}}"></script>
+@endsection
+
+@section('content')
+{{--  --}}
+@endsection
