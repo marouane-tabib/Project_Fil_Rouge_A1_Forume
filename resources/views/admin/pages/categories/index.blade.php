e7aae75d87c8c8a67fc4896cbe41333275b0c0ad
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-21 01:22:40 +0000

    Add the categories name in the table, Add the categories route name

--- resources/views/admin/pages/categories/index.blade.php
+++ resources/views/admin/pages/categories/index.blade.php
@@ -24,14 +24,14 @@
     <tbody class="table-border-bottom-0">
       @foreach ($categories as $category)
         <tr>
-          <td>{{ $category->title }}</td>
+          <td>{{ $category->name }}</td>
           <td>
             <div class="dropdown">
               <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical"></i></button>
               <div class="dropdown-menu">
-                <a class="dropdown-item" href="{{ route('category.edit', $category->id) }}"><i class="ti ti-pencil me-1"></i>Edit</a>
+                <a class="dropdown-item" href="{{ route('categories.edit', $category->id) }}"><i class="ti ti-pencil me-1"></i>Edit</a>
                 <a class="dropdown-item" href="javascript:void(0);" onclick="if(confirm('Are You sure to delete this record?')){document.getElementById('delete-category-{{ $category->id }}').submit();} else {return false}"><i class="ti ti-trash me-1"></i>Delete</a>
-                <form action="{{ route('category.destroy' , $category->id) }}" method="post" class="d-none" id="delete-category-{{ $category->id }}" >
+                <form action="{{ route('categories.destroy' , $category->id) }}" method="post" class="d-none" id="delete-category-{{ $category->id }}" >
                   @csrf
                   @method('DELETE')
                 </form>
