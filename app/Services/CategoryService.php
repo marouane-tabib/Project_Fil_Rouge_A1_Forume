c26f623aa0ae8dffe7b1e6998e823356e1d83552
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-21 00:58:03 +0000

    Add the all|get|find|create|update|delete methods in the CategoryService.php file

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -12,4 +12,34 @@ class CategoryService
   {
     $this->categoryRepository = $categoryRepository;
   }
+
+  public function all()
+  {
+    return $this->categoryRepository->all();
+  }
+
+  public function get(array $select = ['*'], array $with = [])
+  {
+    return $this->categoryRepository->get($select, $with);
+  }
+
+  public function find(int $id)
+  {
+    return $this->categoryRepository->find($id);
+  }
+
+  public function create(array $data)
+  {
+    return $this->categoryRepository->create($data);
+  }
+
+  public function update(int $id, array $data)
+  {
+    return $this->categoryRepository->update($id, $data);
+  }
+
+  public function delete(int $id)
+  {
+    return $this->categoryRepository->delete($id);
+  }
 }
