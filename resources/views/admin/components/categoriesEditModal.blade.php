292b4917f2c75083cb99e554e9c65b6a50b39005
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-21 01:52:39 +0000

    Change the form data structur in the categoriesEditModal.blade

--- resources/views/admin/components/categoriesEditModal.blade.php
+++ resources/views/admin/components/categoriesEditModal.blade.php
@@ -7,27 +7,27 @@
 <!-- Modal -->
 <div class="modal fade" id="modalCenter" tabindex="-1" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
-  <form action="{{ route('categories.store') }}" method="post">
-    @csrf
-    @method('POST')
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="modalCenterTitle">Add New Category</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <div class="row">
-          <div class="col mb-3">
-            <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}" id="name" name="name" placeholder="Add your category name" />
+      <form action="{{ route('categories.store') }}" method="post">
+        @csrf
+        @method('POST')
+        <div class="modal-header">
+          <h5 class="modal-title" id="modalCenterTitle">Add New Category</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col mb-3">
+              <label for="name" class="form-label">Name</label>
+              <input type="text" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}" id="name" name="name" placeholder="Add your category name" />
+            </div>
           </div>
         </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-label-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Send</button>
-      </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-label-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Send</button>
+        </div>
+      </form>
     </div>
-  </form>
   </div>
 </div>
