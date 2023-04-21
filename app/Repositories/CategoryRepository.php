4396993047c1fd49ff279685da2c15ae5fe0fec1
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-21 00:41:45 +0000

    Make the CategoryRepository.php file

---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\CategoryRepositoryInterface;
+use App\Models\Category;
+
+class CategoryRepository extends BaseRepository implements CategoryRepositoryInterface
+{
+  protected $model;
+
+  public function __construct(Category $model)
+  {
+    $this->model = $model;
+  }
+}
