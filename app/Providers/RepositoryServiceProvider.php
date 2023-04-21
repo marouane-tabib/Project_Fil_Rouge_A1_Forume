3ad806949aa1f101f18d27a0fdd346ebfe2ac35f
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-21 16:00:26 +0000

    Add the binding TagRepositoryInterface and TagRepository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-use App\Interfaces\CategoryRepositoryInterface;
+use App\Interfaces\TagRepositoryInterface;
+use App\Repositories\TagRepository;use App\Interfaces\CategoryRepositoryInterface;
 use App\Repositories\CategoryRepository;use App\Interfaces\BaseRepositoryInterface;
 use App\Interfaces\ProductRepositoryInterface;
 use App\Repositories\BaseRepository;
@@ -18,6 +19,7 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register()
     {
+      $this->app->bind(TagRepositoryInterface::class, TagRepository::class);
       $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
       $this->app->bind(BaseRepositoryInterface::class, BaseRepository::class);
       $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
