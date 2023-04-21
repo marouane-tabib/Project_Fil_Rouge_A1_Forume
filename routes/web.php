0d70f752737d6af666b291e7afce8ce96bc29244
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-21 01:16:57 +0000

    Add the categories route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AdminControllers\CategoryController;
 use App\Http\Controllers\AdminControllers\ProductController;
 use App\Http\Controllers\Test\test;
 use Illuminate\Support\Facades\Route;
 
 // admine
 Route::resource('/product', ProductController::class);
+Route::resource('/product', CategoryController::class);
