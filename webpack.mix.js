976771cc00295dd38d2505e7a3cb005a7c26d777
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-21 17:29:40 +0000

    Add the tag assets resource in the webpack.mix.js file to mix it, and building all of mixing assets to public derictory

--- webpack.mix.js
+++ webpack.mix.js
@@ -134,7 +134,8 @@ mix.js('resources/js/laravel-user-management.js', 'public/js/');
 mix.js('resources/admin/assets/js/products/create-validation.js', 'public/admin/assets/js/products/')
    .js('resources/admin/assets/js/products/edit-validation.js', 'public/admin/assets/js/products/')
    .js('resources/admin/assets/js/products/data-form-validation.js', 'public/admin/assets/js/products/')
-   .js('resources/admin/assets/js/categories/data-form-validation.js', 'public/admin/assets/js/categories/');
+   .js('resources/admin/assets/js/categories/data-form-validation.js', 'public/admin/assets/js/categories/')
+   .js('resources/admin/assets/js/tags/data-form-validation.js', 'public/admin/assets/js/tags/');
 
 mix.copy('node_modules/@fortawesome/fontawesome-free/webfonts/*', 'public/assets/vendor/fonts/fontawesome');
 mix.copy('node_modules/katex/dist/fonts/*', 'public/assets/vendor/libs/quill/fonts');
