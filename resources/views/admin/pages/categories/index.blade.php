29c0346fcf9fbc40d459fdfa85871a909f03dff0
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-21 03:32:49 +0000

    Clean

--- resources/views/admin/pages/categories/index.blade.php
+++ resources/views/admin/pages/categories/index.blade.php
@@ -15,6 +15,7 @@
 <script src="{{asset('assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js')}}"></script>
 <script src="{{asset('assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js')}}"></script>
 @endsection
+
 @section('page-script')
 <script src="{{asset('admin/assets/js/categories/data-form-validation.js')}}"></script>
 @endsection
