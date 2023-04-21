2f5cc0a0b4c6e4f6a3a7d594a8968e9c2b4c7d77
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-21 01:08:19 +0000

    Add the categories data in the table categories/index.blade.php file

---
+++ resources/views/admin/pages/categories/index.blade.php
@@ -0,0 +1,46 @@
+@php
+ $configData = Helper::appClasses();
+@endphp
+
+@extends('admin/layouts/layoutMaster')
+
+@section('title', 'Categories')
+
+@section('content')
+<div class="d-flex justify-content-end">
+  <a href="#">
+    <button type="button" class="btn btn-primary">
+      Add Category
+    </button>
+  </a>
+</div><br>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Name</th>
+      </tr>
+    </thead>
+    <tbody class="table-border-bottom-0">
+      @foreach ($categories as $category)
+        <tr>
+          <td>{{ $category->title }}</td>
+          <td>
+            <div class="dropdown">
+              <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical"></i></button>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="{{ route('category.edit', $category->id) }}"><i class="ti ti-pencil me-1"></i>Edit</a>
+                <a class="dropdown-item" href="javascript:void(0);" onclick="if(confirm('Are You sure to delete this record?')){document.getElementById('delete-category-{{ $category->id }}').submit();} else {return false}"><i class="ti ti-trash me-1"></i>Delete</a>
+                <form action="{{ route('category.destroy' , $category->id) }}" method="post" class="d-none" id="delete-category-{{ $category->id }}" >
+                  @csrf
+                  @method('DELETE')
+                </form>
+              </div>
+            </div>
+          </td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
+</div>
+@endsection
