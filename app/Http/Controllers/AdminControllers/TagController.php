c656c1486be6144b7b4652540010bd571fee7d3c
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-21 16:09:04 +0000

    Add the view tag.index return in the index method, in the TagController

--- app/Http/Controllers/AdminControllers/TagController.php
+++ app/Http/Controllers/AdminControllers/TagController.php
@@ -22,7 +22,7 @@ class TagController extends Controller
      */
     public function index()
     {
-        //
+      return view('admin.pages.tags.index', ['tags' => $this->tagService->get()]);
     }
 
     /**
