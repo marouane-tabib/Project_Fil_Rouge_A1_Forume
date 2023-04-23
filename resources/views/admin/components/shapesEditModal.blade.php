f84bbac1599e3b4ed5e301ff070fe02e30106580
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-23 13:03:46 +0000

    Change the modal title in the shapesEditModal

--- resources/views/admin/components/shapesEditModal.blade.php
+++ resources/views/admin/components/shapesEditModal.blade.php
@@ -12,7 +12,7 @@
         @csrf
         @method('POST')
         <div class="modal-header">
-          <h5 class="modal-title" id="modalCenterTitle">Add New Category</h5>
+          <h5 class="modal-title" id="modalCenterTitle">Add New Shape</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
