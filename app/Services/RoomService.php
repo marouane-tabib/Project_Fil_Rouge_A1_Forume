e4d1d77453b569edf0d523e73b3812c9368ea349
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-23 16:38:18 +0000

    Add the methods all|get|find|create|update|delete in the RoomService

--- app/Services/RoomService.php
+++ app/Services/RoomService.php
@@ -12,4 +12,34 @@ class RoomService
   {
     $this->roomRepository = $roomRepository;
   }
+
+  public function all()
+  {
+    return $this->roomRepository->all();
+  }
+
+  public function get(array $select = ['*'], array $with = [])
+  {
+    return $this->roomRepository->get($select, $with);
+  }
+
+  public function find(int $id)
+  {
+    return $this->roomRepository->find($id);
+  }
+
+  public function create(array $data)
+  {
+    return $this->roomRepository->create($data);
+  }
+
+  public function update(int $id, array $data)
+  {
+    return $this->roomRepository->update($id, $data);
+  }
+
+  public function delete(int $id)
+  {
+    return $this->roomRepository->delete($id);
+  }
 }
