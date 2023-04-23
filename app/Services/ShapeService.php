cb6ad0641a956b61965780fa42a3d202715a8ac3
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-23 12:46:36 +0000

    Add the methods all|get|find|create|update|delete in the ShapeService.php file

--- app/Services/ShapeService.php
+++ app/Services/ShapeService.php
@@ -12,4 +12,34 @@ class ShapeService
   {
     $this->shapeRepository = $shapeRepository;
   }
+
+  public function all()
+  {
+    return $this->shapeRepository->all();
+  }
+
+  public function get(array $select = ['*'], array $with = [])
+  {
+    return $this->shapeRepository->get($select, $with);
+  }
+
+  public function find(int $id)
+  {
+    return $this->shapeRepository->find($id);
+  }
+
+  public function create(array $data)
+  {
+    return $this->shapeRepository->create($data);
+  }
+
+  public function update(int $id, array $data)
+  {
+    return $this->shapeRepository->update($id, $data);
+  }
+
+  public function delete(int $id)
+  {
+    return $this->shapeRepository->delete($id);
+  }
 }
