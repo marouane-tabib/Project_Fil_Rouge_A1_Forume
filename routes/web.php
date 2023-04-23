8f404f6026670457f5ec8258b99bf5c0591ae718
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-23 16:49:42 +0000

    Add the rooms route resource with RoomController in the routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminControllers\CategoryController;
 use App\Http\Controllers\AdminControllers\ProductController;
+use App\Http\Controllers\AdminControllers\RoomController;
 use App\Http\Controllers\AdminControllers\ShapeController;
 use App\Http\Controllers\AdminControllers\TagController;
 use App\Http\Controllers\Test\test;
@@ -12,3 +13,4 @@ Route::resource('/products', ProductController::class);
 Route::resource('/categories', CategoryController::class);
 Route::resource('/tags', TagController::class);
 Route::resource('/shapes', ShapeController::class);
+Route::resource('/rooms', RoomController::class);
