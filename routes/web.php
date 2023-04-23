171850fd6ff7d9c0583e4ddf9c96f6b19ffc72d1
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-23 13:04:36 +0000

    Change the shapes route resource Controller, to ShapeController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminControllers\CategoryController;
 use App\Http\Controllers\AdminControllers\ProductController;
+use App\Http\Controllers\AdminControllers\ShapeController;
 use App\Http\Controllers\AdminControllers\TagController;
 use App\Http\Controllers\Test\test;
 use Illuminate\Support\Facades\Route;
@@ -10,4 +11,4 @@ use Illuminate\Support\Facades\Route;
 Route::resource('/products', ProductController::class);
 Route::resource('/categories', CategoryController::class);
 Route::resource('/tags', TagController::class);
-Route::resource('/shapes', TagController::class);
+Route::resource('/shapes', ShapeController::class);
