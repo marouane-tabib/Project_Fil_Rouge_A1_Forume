a87c0519742420277d48d8cbb5bd96d582898957
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-23 16:35:34 +0000

    Add the binding the RoomRepositoryInterface and the RoomRepository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-use App\Interfaces\ShapeRepositoryInterface;
+use App\Interfaces\RoomRepositoryInterface;
+use App\Repositories\RoomRepository;use App\Interfaces\ShapeRepositoryInterface;
 use App\Repositories\ShapeRepository;use App\Interfaces\TagRepositoryInterface;
 use App\Repositories\TagRepository;use App\Interfaces\CategoryRepositoryInterface;
 use App\Repositories\CategoryRepository;use App\Interfaces\BaseRepositoryInterface;
@@ -20,6 +21,7 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register()
     {
+      $this->app->bind(RoomRepositoryInterface::class, RoomRepository::class);
       $this->app->bind(ShapeRepositoryInterface::class, ShapeRepository::class);
       $this->app->bind(TagRepositoryInterface::class, TagRepository::class);
       $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
