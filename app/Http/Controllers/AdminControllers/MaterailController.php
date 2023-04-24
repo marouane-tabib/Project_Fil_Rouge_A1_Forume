588a6e8f9742ad4b16f0afba01289ef122db1ee3
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-24 23:54:30 +0100

    Add the store method in the MaterailController

--- app/Http/Controllers/AdminControllers/MaterailController.php
+++ app/Http/Controllers/AdminControllers/MaterailController.php
@@ -43,7 +43,8 @@ class MaterailController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $this->materailService->create($request->all());
+      return redirect()->back();
     }
 
     /**
