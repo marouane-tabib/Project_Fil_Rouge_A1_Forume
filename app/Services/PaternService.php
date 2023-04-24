f70c2396e0c08ae908e7b2e17c1c7fe5db3f0e29
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-24 00:08:42 +0000

    Add the methods all|get|find|create|update|delete in the PaternService

--- app/Services/PaternService.php
+++ app/Services/PaternService.php
@@ -12,4 +12,34 @@ class PaternService
   {
     $this->paternRepository = $paternRepository;
   }
+
+  public function all()
+  {
+    return $this->paternRepository->all();
+  }
+
+  public function get(array $select = ['*'], array $with = [])
+  {
+    return $this->paternRepository->get($select, $with);
+  }
+
+  public function find(int $id)
+  {
+    return $this->paternRepository->find($id);
+  }
+
+  public function create(array $data)
+  {
+    return $this->paternRepository->create($data);
+  }
+
+  public function update(int $id, array $data)
+  {
+    return $this->paternRepository->update($id, $data);
+  }
+
+  public function delete(int $id)
+  {
+    return $this->paternRepository->delete($id);
+  }
 }
