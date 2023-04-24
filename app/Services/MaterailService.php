cc2324d5336d20c3cf4d318917bd2a9b26b90e15
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-24 22:04:30 +0100

    Add the methods all|get|find|create|update|delete in tha MaterailService

--- app/Services/MaterailService.php
+++ app/Services/MaterailService.php
@@ -12,5 +12,34 @@ class MaterailService
   {
     $this->materailRepository = $materailRepository;
   }
-  
+
+  public function all()
+  {
+    return $this->materailRepository->all();
+  }
+
+  public function get(array $select = ['*'], array $with = [])
+  {
+    return $this->materailRepository->get($select, $with);
+  }
+
+  public function find(int $id)
+  {
+    return $this->materailRepository->find($id);
+  }
+
+  public function create(array $data)
+  {
+    return $this->materailRepository->create($data);
+  }
+
+  public function update(int $id, array $data)
+  {
+    return $this->materailRepository->update($id, $data);
+  }
+
+  public function delete(int $id)
+  {
+    return $this->materailRepository->delete($id);
+  }
 }
