630e44cf37b576a7c3f805716eb17b2aaf105db0
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-27 17:11:33 +0100

    Make the resources/views/admin/components/colorsEditModal.blade.php file

---
+++ resources/views/admin/components/colorsEditModal.blade.php
@@ -0,0 +1,36 @@
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalCenter">
+  Add Color
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="modalCenter" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <form action="{{ route('colors.store') }}" method="post" id="createColorForm">
+        @csrf
+        @method('POST')
+        <div class="modal-header">
+          <h5 class="modal-title" id="modalCenterTitle">Add New Color</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col">
+              <label for="name" class="form-label">Name</label>
+              <input type="text" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}" id="name" name="name" placeholder="Add your color name" />
+              @error('name')
+                <span class="text-danger">{{$message}}</span>
+              @enderror
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-label-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Send</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
