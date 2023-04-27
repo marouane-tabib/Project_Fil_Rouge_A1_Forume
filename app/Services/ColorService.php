304768125a453733c7a72c364584fcf9f52899d3
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-27 16:51:53 +0100

    Add the methods all|get|find|create|update|delete in the ColorService

--- app/Services/ColorService.php
+++ app/Services/ColorService.php
@@ -12,4 +12,34 @@ class ColorService
   {
     $this->colorRepository = $colorRepository;
   }
+
+  public function all()
+  {
+    return $this->colorRepository->all();
+  }
+
+  public function get(array $select = ['*'], array $with = [])
+  {
+    return $this->colorRepository->get($select, $with);
+  }
+
+  public function find(int $id)
+  {
+    return $this->colorRepository->find($id);
+  }
+
+  public function create(array $data)
+  {
+    return $this->colorRepository->create($data);
+  }
+
+  public function update(int $id, array $data)
+  {
+    return $this->colorRepository->update($id, $data);
+  }
+
+  public function delete(int $id)
+  {
+    return $this->colorRepository->delete($id);
+  }
 }
