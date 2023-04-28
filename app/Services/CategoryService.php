1b796b96de472fb74e9d29d5b0a2726236c0355f
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-28 23:00:12 +0100

    Clean

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -2,10 +2,8 @@
 
 namespace App\Services;
 
-use App\Interfaces\BaseRepositoryInterface;
 use App\Interfaces\CategoryRepositoryInterface;
 use App\Interfaces\ServiceInterfaces\CategoryServiceInterface;
-use App\Repositories\CategoryRepository;
 
 class CategoryService extends BaseResourceService implements CategoryServiceInterface
 {
