25256747d9d76e3c3792e532776ae7a8257e43ef
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-28 17:57:17 +0100

    Clean

--- app/Services/BaseResourceService.php
+++ app/Services/BaseResourceService.php
@@ -18,22 +18,27 @@ class BaseResourceService implements BaseResourceServiceInterface
   {
     return $this->repository->all();
   }
+
   public function store(array $data)
   {
     //
   }
+  
   public function show(int $id)
   {
     //
   }
+  
   public function edit(int $id)
   {
     //
   }
+  
   public function update(int $id, array $data)
   {
     //
   }
+  
   public function destroy(int $id)
   {
     //
