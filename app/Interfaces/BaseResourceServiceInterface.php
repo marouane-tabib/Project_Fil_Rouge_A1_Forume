f614038dfb69e1a6c11678c5d78a39477a5d0a14
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-28 15:59:16 +0100

    Add the implemention all|create|store|show|edit|update methods in the BaseResourceServiceInterface.php

--- app/Interfaces/BaseResourceServiceInterface.php
+++ app/Interfaces/BaseResourceServiceInterface.php
@@ -2,7 +2,13 @@
 
 namespace App\Interfaces;
 
-interface BaseResourceServiceInterface extends BaseRepositoryInterface
+interface BaseResourceServiceInterface
 {
-    //
+    // all - create - store - show - edit - update - destroy
+  public function all();
+  public function store(array $data);
+  public function show(int $id);
+  public function edit(int $id);
+  public function update(int $id, array $data);
+  public function destroy(int $id);
 }
