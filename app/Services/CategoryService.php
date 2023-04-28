dcb8dbca762e88a2ab248189a7429075088c27ea
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-28 22:58:55 +0100

    Add the injection the $repository argument with CategoryRepositoryInterface type| work with the methods of the BaseResourceService

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -2,44 +2,53 @@
 
 namespace App\Services;
 
+use App\Interfaces\BaseRepositoryInterface;
 use App\Interfaces\CategoryRepositoryInterface;
+use App\Interfaces\ServiceInterfaces\CategoryServiceInterface;
+use App\Repositories\CategoryRepository;
 
-class CategoryService
+class CategoryService extends BaseResourceService implements CategoryServiceInterface
 {
-  protected CategoryRepositoryInterface $categoryRepository;
+  protected $repository;
 
-  public function __construct(CategoryRepositoryInterface $categoryRepository)
+  public function __construct(CategoryRepositoryInterface $repository)
   {
-    $this->categoryRepository = $categoryRepository;
+    $this->repository = $repository;
   }
 
-  public function all()
-  {
-    return $this->categoryRepository->all();
-  }
 
-  public function get(array $select = ['*'], array $with = [])
-  {
-    return $this->categoryRepository->get($select, $with);
-  }
+  // public function __construct(CategoryRepositoryInterface $categoryRepository)
+  // {
+  //   $this->categoryRepository = $categoryRepository;
+  // }
 
-  public function find(int $id)
-  {
-    return $this->categoryRepository->find($id);
-  }
+  // public function all()
+  // {
+  //   return $this->categoryRepository->all();
+  // }
 
-  public function create(array $data)
-  {
-    return $this->categoryRepository->create($data);
-  }
+  // public function get(array $select = ['*'], array $with = [])
+  // {
+  //   return $this->categoryRepository->get($select, $with);
+  // }
 
-  public function update(int $id, array $data)
-  {
-    return $this->categoryRepository->update($id, $data);
-  }
+  // public function find(int $id)
+  // {
+  //   return $this->categoryRepository->find($id);
+  // }
 
-  public function delete(int $id)
-  {
-    return $this->categoryRepository->delete($id);
-  }
+  // public function create(array $data)
+  // {
+  //   return $this->categoryRepository->create($data);
+  // }
+
+  // public function update(int $id, array $data)
+  // {
+  //   return $this->categoryRepository->update($id, $data);
+  // }
+
+  // public function delete(int $id)
+  // {
+  //   return $this->categoryRepository->delete($id);
+  // }
 }
