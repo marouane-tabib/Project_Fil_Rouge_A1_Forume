4db0bad8afc5e870a2b51fd560c968dab3045574
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-28 23:47:49 +0100

    Change the injecting repository propretie type and commant all of the methods in the ColorService

--- app/Services/ColorService.php
+++ app/Services/ColorService.php
@@ -3,43 +3,44 @@
 namespace App\Services;
 
 use App\Interfaces\ColorRepositoryInterface;
+use App\Interfaces\ServiceInterfaces\ColorServiceInterface;
 
-class ColorService
+class ColorService extends BaseResourceService implements ColorServiceInterface
 {
-  protected ColorRepositoryInterface $colorRepository;
+  protected $repository;
 
-  public function __construct(ColorRepositoryInterface $colorRepository)
+  public function __construct(ColorRepositoryInterface $repository)
   {
-    $this->colorRepository = $colorRepository;
+    $this->repository = $repository;
   }
 
-  public function all()
-  {
-    return $this->colorRepository->all();
-  }
+  // public function all()
+  // {
+  //   return $this->colorRepository->all();
+  // }
 
-  public function get(array $select = ['*'], array $with = [])
-  {
-    return $this->colorRepository->get($select, $with);
-  }
+  // public function get(array $select = ['*'], array $with = [])
+  // {
+  //   return $this->colorRepository->get($select, $with);
+  // }
 
-  public function find(int $id)
-  {
-    return $this->colorRepository->find($id);
-  }
+  // public function find(int $id)
+  // {
+  //   return $this->colorRepository->find($id);
+  // }
 
-  public function create(array $data)
-  {
-    return $this->colorRepository->create($data);
-  }
+  // public function create(array $data)
+  // {
+  //   return $this->colorRepository->create($data);
+  // }
 
-  public function update(int $id, array $data)
-  {
-    return $this->colorRepository->update($id, $data);
-  }
+  // public function update(int $id, array $data)
+  // {
+  //   return $this->colorRepository->update($id, $data);
+  // }
 
-  public function delete(int $id)
-  {
-    return $this->colorRepository->delete($id);
-  }
+  // public function delete(int $id)
+  // {
+  //   return $this->colorRepository->delete($id);
+  // }
 }
