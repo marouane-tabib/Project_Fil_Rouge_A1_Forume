9d38e9a70e4d9eb9f0ec59c6da66956a2b3498ae
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-29 04:01:33 +0100

    Clean the HomeStyleService.php

--- app/Services/HomeStyleService.php
+++ app/Services/HomeStyleService.php
@@ -13,34 +13,4 @@ class HomeStyleService extends BaseResourceService implements HomeStyleServiceIn
   {
     $this->repository = $repository;
   }
-
-  // public function all()
-  // {
-  //   return $this->homestyleRepository->all();
-  // }
-
-  // public function get(array $select = ['*'], array $with = [])
-  // {
-  //   return $this->homestyleRepository->get($select, $with);
-  // }
-
-  // public function find(int $id)
-  // {
-  //   return $this->homestyleRepository->find($id);
-  // }
-
-  // public function create(array $data)
-  // {
-  //   return $this->homestyleRepository->create($data);
-  // }
-
-  // public function update(int $id, array $data)
-  // {
-  //   return $this->homestyleRepository->update($id, $data);
-  // }
-
-  // public function delete(int $id)
-  // {
-  //   return $this->homestyleRepository->delete($id);
-  // }
 }
