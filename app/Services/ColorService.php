f396afb9715e2da4eabc0a368fefa30f8fc1cae7
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-29 03:48:58 +0100

    Clean

--- app/Services/ColorService.php
+++ app/Services/ColorService.php
@@ -13,34 +13,4 @@ class ColorService extends BaseResourceService implements ColorServiceInterface
   {
     $this->repository = $repository;
   }
-
-  // public function all()
-  // {
-  //   return $this->colorRepository->all();
-  // }
-
-  // public function get(array $select = ['*'], array $with = [])
-  // {
-  //   return $this->colorRepository->get($select, $with);
-  // }
-
-  // public function find(int $id)
-  // {
-  //   return $this->colorRepository->find($id);
-  // }
-
-  // public function create(array $data)
-  // {
-  //   return $this->colorRepository->create($data);
-  // }
-
-  // public function update(int $id, array $data)
-  // {
-  //   return $this->colorRepository->update($id, $data);
-  // }
-
-  // public function delete(int $id)
-  // {
-  //   return $this->colorRepository->delete($id);
-  // }
 }
