2a8b204685860f4145a65e949357452177a5d20f
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-30 17:09:33 +0100

    Extend the BaseResourceService and implement the TagServiceInterface in the TagService

--- app/Services/TagService.php
+++ app/Services/TagService.php
@@ -2,44 +2,15 @@
 
 namespace App\Services;
 
+use App\Interfaces\ServiceInterfaces\TagServiceInterface;
 use App\Interfaces\TagRepositoryInterface;
 
-class TagService
+class TagService extends BaseResourceService implements TagServiceInterface
 {
-  protected TagRepositoryInterface $tagRepository;
+  protected $repository;
 
-  public function __construct(TagRepositoryInterface $tagRepository)
+  public function __construct(TagRepositoryInterface $repository)
   {
-    $this->tagRepository = $tagRepository;
-  }
-
-  public function all()
-  {
-    return $this->tagRepository->all();
-  }
-
-  public function get(array $select = ['*'], array $with = [])
-  {
-    return $this->tagRepository->get($select, $with);
-  }
-
-  public function find(int $id)
-  {
-    return $this->tagRepository->find($id);
-  }
-
-  public function create(array $data)
-  {
-    return $this->tagRepository->create($data);
-  }
-
-  public function update(int $id, array $data)
-  {
-    return $this->tagRepository->update($id, $data);
-  }
-
-  public function delete(int $id)
-  {
-    return $this->tagRepository->delete($id);
+    $this->repository = $repository;
   }
 }
