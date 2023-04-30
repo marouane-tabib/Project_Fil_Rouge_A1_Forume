36a048b69912d83163993412ea83786c0bb188a7
marwan-tabib <marwanmaroc297@gmail.com>
2023-04-30 14:24:09 +0100

    Add the binding with the MaterialServiceInterface and the MaterialService classes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,10 +6,12 @@ use App\Interfaces\ServiceInterfaces\BaseResourceServiceInterface;
 use App\Interfaces\ServiceInterfaces\CategoryServiceInterface;
 use App\Interfaces\ServiceInterfaces\ColorServiceInterface;
 use App\Interfaces\ServiceInterfaces\HomeStyleServiceInterface;
+use App\Interfaces\ServiceInterfaces\MaterialServiceInterface;
 use App\Services\BaseResourceService;
 use App\Services\CategoryService;
 use App\Services\ColorService;
 use App\Services\HomeStyleService;
+use App\Services\MaterialService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
    */
   public function register()
   {
+    $this->app->bind(MaterialServiceInterface::class, MaterialService::class);
     $this->app->bind(HomeStyleServiceInterface::class, HomeStyleService::class);
     $this->app->bind(ColorServiceInterface::class, ColorService::class);
     $this->app->bind(CategoryServiceInterface::class, CategoryService::class);
