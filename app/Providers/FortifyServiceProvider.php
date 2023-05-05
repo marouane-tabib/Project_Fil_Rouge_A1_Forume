7e308a97bff05e307c23c5ddc5e65b7ea88ceb89
marwan-tabib <marwanmaroc297@gmail.com>
2023-05-05 17:21:09 +0100

    Add the the reques validation rules and the validation errors message in the RateLimiter provider from the FortifyServiceProvider

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -6,12 +6,16 @@ use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
+use App\Http\Requests\LoginRequest as LoginVlidator;
 use App\Http\Requests\UserRequest;
+use App\Models\User;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
+use Laravel\Fortify\Http\Requests\LoginRequest;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -36,20 +40,30 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::loginView(function () {
           return view('admin.auth.login');
         });
-        // RateLimiter::for('login', function (Request $request) {
-        //     $email = (string) $request->email;
 
-        //     return Limit::perMinute(5)->by($email.$request->ip());
-        // });
+        RateLimiter::for('login', function (Request $request) {
+            $validator = $request->validate([
+              'email' => ['required', 'string', 'email', 'min:4', 'max:150', 'exists:users,email'],
+              'password' => ['required', 'string', 'min:8', 'max:150'],
+              'remember' => ['nullable', 'boolean'],
+            ],  [
+              'email.required' => 'Please enter your email address.',
+              'email.string' => 'Your email address must be a string.',
+              'email.email' => 'Please enter a valid email address.',
+              'email.min' => 'Your email address must be at least 4 characters long.',
+              'email.max' => 'Your email address must not exceed 150 characters.',
+              'email.exists' => 'The email you entered is not registered',
+              'password.required' => 'Please enter your password.',
+              'password.string' => 'Your password must be a string.',
+              'password.min' => 'Your password must be at least 8 characters long.',
+              'password.max' => 'Your password must not exceed 150 characters.',
+              'remember.boolean' => 'The remember field must be either true or false.',
+          ]);
 
-        Fortify::authenticateUsing(function (UserRequest $request) {
-          $user = User::where('email', $request->email)->first();
+            $email = (string) $validator['email'];
 
-          if ($user &&
-              Hash::check($request->password, $user->password)) {
-              return $user;
-          }
-      });
+            return Limit::perMinute(5)->by($email.$request->ip());
+        });
 
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
