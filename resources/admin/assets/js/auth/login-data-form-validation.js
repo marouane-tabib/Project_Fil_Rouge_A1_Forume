5c6c1c45bc5774bdeee57ea387573bb718fc8d61
marwan-tabib <marwanmaroc297@gmail.com>
2023-05-06 18:36:36 +0100

    Change the name the resources/admin/assets/js/auth/data-form-validation.js file to login-data-form-validation | Chnage the login rules validation

---
+++ resources/admin/assets/js/auth/login-data-form-validation.js
@@ -0,0 +1,74 @@
+/**
+ *  Pages Authentication
+ */
+
+'use strict';
+const formAuthentication = document.querySelector('#loginFromValidation');
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  (function () {
+    // Form validation for Add new record
+    if (formAuthentication) {
+      const fv = FormValidation.formValidation(formAuthentication, {
+        fields: {
+          email: {
+            validators: {
+              notEmpty: {
+                message: 'Please enter your email address.'
+              },
+              emailAddress: {
+                message: 'Please enter valid email address'
+              },
+              stringLength: {
+                max: 150,
+                message: 'The email must not be greater than 150 characters.'
+              }
+            }
+          },
+          password: {
+            validators: {
+              notEmpty: {
+                message: 'Please enter your password'
+              },
+              stringLength: {
+                min: 8,
+                max: 150,
+                message: 'The pattern name must be between 8 and 150 characters in length.'
+              }
+            }
+          }
+        },
+        plugins: {
+          trigger: new FormValidation.plugins.Trigger(),
+          bootstrap5: new FormValidation.plugins.Bootstrap5({
+            eleValidClass: '',
+            rowSelector: '.mb-3'
+          }),
+          submitButton: new FormValidation.plugins.SubmitButton(),
+
+          defaultSubmit: new FormValidation.plugins.DefaultSubmit(),
+          autoFocus: new FormValidation.plugins.AutoFocus()
+        },
+        init: instance => {
+          instance.on('plugins.message.placed', function (e) {
+            if (e.element.parentElement.classList.contains('input-group')) {
+              e.element.parentElement.insertAdjacentElement('afterend', e.messageElement);
+            }
+          });
+        }
+      });
+    }
+
+    //  Two Steps Verification
+    const numeralMask = document.querySelectorAll('.numeral-mask');
+
+    // Verification masking
+    if (numeralMask.length) {
+      numeralMask.forEach(e => {
+        new Cleave(e, {
+          numeral: true
+        });
+      });
+    }
+  })();
+});
