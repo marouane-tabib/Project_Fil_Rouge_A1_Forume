2700cdc680f25ca3b3b54b0fa0920c3f9ca9fbb4
marwan-tabib <marwanmaroc297@gmail.com>
2023-05-07 23:13:26 +0100

    Restore the username column in the fillable User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
       'first_name',
       'last_name',
-      // 'username',
+      'username',
       'email',
       'mobile',
       'password',
