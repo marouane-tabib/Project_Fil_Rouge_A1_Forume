ef08b0937942eeb91d5564882fb0a1419de333be
marwan-tabib <marwanmaroc297@gmail.com>
2023-05-07 22:08:17 +0100

    Change the users schema table in the create_users_table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+          $table->id();
+          $table->string('first_name');
+          $table->string('last_name');
+          $table->string('username')->unique();
+          $table->string('email')->unique();
+          $table->timestamp('email_verified_at')->nullable();
+          $table->string('mobile')->unique();
+          $table->string('password');
+          $table->string('user_image')->nullable();
+          $table->unsignedTinyInteger('status')->default(0);
+          $table->rememberToken();
+          $table->timestamps();
         });
     }
 
