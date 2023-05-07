b52953f890b748ffb2e63dfb60a4e121562ccef8
marwan-tabib <marwanmaroc297@gmail.com>
2023-05-07 22:52:13 +0100

    Add the entrust UserPermissions methods and delete the Laravel default model template in the Models/UserPermissions model

--- app/Models/UserPermissions.php
+++ app/Models/UserPermissions.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UserPermissions extends Model
 {
-    use HasFactory;
+    protected $guarded = [];
+
+    public function permission()
+    {
+        return $this->belongsToMany(Permission::class, 'id', 'permission_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsToMany(User::class, 'id', 'user_id');
+    }
 }
