926d8e2d0a4af2dd2c8c0fb462f16d3fbc420f81
marwan-tabib <marwanmaroc297@gmail.com>
2023-05-07 22:29:15 +0100

    Change the users auth provider in the config/auth.php file

--- config/auth.php
+++ config/auth.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+
 return [
 
     /*
@@ -62,7 +64,8 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => User::class,
+            'table' => 'users',
         ],
 
         // 'users' => [
